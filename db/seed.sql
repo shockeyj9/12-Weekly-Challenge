86783c0e206b1431c5bdfb01280f59691066a017
Jessie Shockey <shockeyjessie92@gmail.com>
2023-09-20 18:16:54 -0600

    Added seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,18 +1,32 @@
 INSERT INTO department ( name)
 VALUES
-    (  ""),
-    (  ""),
-    (  "");
+    ("Accounting"),
+    ("Administration"),
+    ("Marketing"),
+    ("Human Resources"),
+    ("Customer Service");
 
 INSERT INTO role ( title,salary,department_id)
 VALUES
-    (  ),
-    (  ),
-    (  );
+    ("Tax Accountant",57418,1),
+    ("General Accountant", 57418, 1),
+    ("Administrative Manager",129691, 2),
+    ("VP of Administration",129691, 2),
+    ("Chief Administrative Officer",129691, 2),
+    ("Marketing specialist",67054,3),
+    ("HR Specialist",70209,4),
+    ("Customer Support Specialist", 54471, 5)
+    ;
 
-INSERT INTO employee ( first_name,last_name,role_id,manager_id)
+INSERT INTO employee ( first_name,last_name,role_id, manager_id)
 VALUES
-    (  ),
-    (  ),
-    (  );
+    ("Theudemar", "Africa", 4, NULL), 
+    ("Engelbert", "Joscelin", 5, 1), 
+    ("Walt","Rahim",3, 2), 
+    ("Omar", "Jem", 1,3),
+    ("Venkat","Ida", 2,3),
+    ("Diksha", "Kebede", 6,3),
+    ("Ann" ,"Gael", 7,3),
+    ("Nina" ,"Adela",8,3)
+    ;
 
